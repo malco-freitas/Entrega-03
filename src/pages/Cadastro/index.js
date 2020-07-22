c4e98403df69d6d8c080615789885a435f8769ad
Malco Freitas <malco.eletronica@poli.ufrj>
2020-07-21 23:29:29 -0300

    INSERÇÃO DE INPUTS EM TELA DE CADASTRO

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -8,10 +8,13 @@ class Cadastro extends Component {
                 <div className="caixaCadastro">
                     <h1>Novo Cadastro</h1>
                     <h2>Digite um nome de Login:</h2>
+                    <input type="text"/>
                     <div className="entradaLogin"></div>
                     <h2>Digite uma senha:</h2>
+                    <input type="password"/>
                     <div className="entradaSenha"></div>
                     <h2>Repita sua senha:</h2>
+                    <input type="password"/>
                     <div className="entradaSenha"></div>
                     <div className="botaoLogin">Login</div>
                 </div>
